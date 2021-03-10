c60a406495c97a8be9b07bc8a5467786968f7b2c
csis2020 <koh11@student.douglascollege.ca>
2021-03-09 18:54:23 -0800

    Added comment. It is just for testing to use Git by Hye Kyung Ko.  It will removed later.

--- app/src/main/java/com/example/recipefinder/MainActivity.java
+++ app/src/main/java/com/example/recipefinder/MainActivity.java
@@ -26,5 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Test to push from Hye Kyung Ko local repo
     }
 }
