fa2c3ed13ce66cc65388bd8a553317a8f2c8af78
Tabitha Maru <tabitha.maru@gmail.com>
2021-03-13 23:05:20 -0800

    Try push

--- app/src/main/java/com/example/recipefinder/MainActivity.java
+++ app/src/main/java/com/example/recipefinder/MainActivity.java
@@ -25,6 +25,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     // list for ingredients from csv to adapter
+
     List<String> ingredientsList = new ArrayList<>(Arrays.asList());
 
     @Override
